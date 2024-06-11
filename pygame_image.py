a1abc0b67c121d974ea15e838cf3d0427472e7da
Tsuboderaaaa_c0a23052 <coa2305216@edu.teu.ac.jp>
2024-06-11 16:15:46 +0900

    練習問題５　FPS変更

--- pygame_image.py
+++ pygame_image.py
@@ -10,17 +10,20 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
-    kkt_img = pg.image.load("3.png")
-    kkt_img = pg.transform(kkt_img, True, False)
+    kkt_img = pg.image.load("fig/3.png")
+    kkt_img = pg.transform.flip(kkt_img, True, False)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
+        kkt_rct = kkt_img.get_rect() # こうかとんrectの抽出
+        kkt_rct.center = 300, 200
+        screen.blit(kkt_img, kkt_rct)
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(200)
 
 
 if __name__ == "__main__":
